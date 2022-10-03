2eef8812306c2ea6e3ff2936f0e2cf4465074420
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-10-03 15:41:02 +0530

    Added Functionality to convert a text or url into Qr Code using Qr Code Api

---
+++ index.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector('.wrapper')
+const btn = document.getElementById('btn')
+const qrInput = document.getElementById('qrInput')
+qrImg = document.querySelector('.qr-code img')
+
+console.log(qrImg.src);
+
+btn.addEventListener('click',()=>{
+
+    let qrValue = qrInput.value
+    if(!qrValue) return;
+    btn.innerText = "Generating QR Code..."
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`
+    
+    qrImg.addEventListener("load",()=>{
+        wrapper.classList.add('active')
+        btn.innerText = "Generate QR Code"
+    })
+    wrapper.classList.add("active");
+   
+})
+
+qrInput.addEventListener("keyup",()=>{
+    if (!qrInput.value) {
+        wrapper.classList.remove('active')
+    }
+})
