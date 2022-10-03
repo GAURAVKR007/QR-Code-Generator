a01b69a85f4544269298b96e0630948966eed875
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-10-03 17:47:03 +0530

    Added Functionality for Copied to Clipboard

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ const wrapper = document.querySelector('.wrapper')
 const btn = document.getElementById('btn')
 const qrInput = document.getElementById('qrInput')
 qrImg = document.querySelector('.qr-code img')
+const copyBtn = document.getElementById('copy')
+const toasts = document.getElementById('toasts')
+
+const message = "Copied To Clipboard"
+
+copyBtn.addEventListener('click',()=>{
+    copyToClipboard(qrImg.src)
+    createNotification()
+})
+
+function createNotification(messagetext = message) {
+    const notif = document.createElement('div')
+    notif.classList.add('toast')
+}
+
+async function copyToClipboard(src) {
+    const data = await fetch(src);
+    const blob = await data.blob();
+
+    try{
+        await navigator.clipboard.write([
+            new ClipboardItem({
+                [blob.type]: blob,
+            })
+        ])
+        console.log("Success");
+    }catch(e) {
+        console.log(e);
+    }
+
+   
+}
 
 console.log(qrImg.src);
 
@@ -17,12 +49,17 @@ btn.addEventListener('click',()=>{
         btn.innerText = "Generate QR Code"
     })
     wrapper.classList.add("active");
+
+    btn.style.width = '84%';
+    copyBtn.classList.add('active')
    
 })
 
 qrInput.addEventListener("keyup",()=>{
     if (!qrInput.value) {
         wrapper.classList.remove('active')
+        btn.style.width = '100%';
+        copyBtn.classList.remove('active')
     }
 })
 
